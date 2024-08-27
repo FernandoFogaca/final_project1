1f88f4b1559909823bcc58a00adab13f601fb4c0
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-08-27 03:15:14 +0200

    feat: adiciona o tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-   <h1>Final Project...</h1>
+    <div className="flex justify-start p-20 font-sans">
+      <h1 className="bg-orange-400 font-bold text-white hover:text-orange-800 transition-all duration-300">
+        Full Stack Developer / Final Project
+      </h1>
+    </div>
+  
+
     </>
   )
 }
