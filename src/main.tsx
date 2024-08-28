93c891e2df9dde4145d6585b61e4021b3d21a025
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-08-28 02:56:09 +0200

    feat: adiciona o primereact

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PrimeReactProvider } from 'primereact/api'
+import 'primeicons/primeicons.css'
 import App from './App.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </StrictMode>,
 )
