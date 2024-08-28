9a10bcfe9124a8433a1b3d5debed6bc4dd51e8b1
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-08-28 22:33:09 +0200

    feat: exercicio 01 - adiciona routers ao projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
-
 import { Button } from 'primereact/button';
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <>
-    <div className="flex justify-start p-5 font-sans">
-      <h1 className="bg-orange-400 p-2  flex-row font-bold text-white border-solid border-black border-2 hover:text-orange-800 transition-all duration-300">
-        Full Stack Developer / Final Project
-      </h1>
-      <Button 
-        label="Like"
-        icon="pi pi-heart-fill"
-        severity='danger'
-
-      />
+    <div className="flex justify-center items-center h-screen font-sans">
+      {/* Container centrado */}
+      <div className="flex flex-col items-center space-y-4">
+        <h1 className="bg-orange-400 p-2 font-bold text-white border-solid border-black border-2 hover:text-orange-800 transition-all duration-300">
+          Full Stack Developer / Final Project
+        </h1>
+        <Button 
+          label="Bienvenue"
+          icon="pi pi-heart-fill"
+          severity='danger'
+        />
+      </div>
     </div>
-    
-
-    
-  
-
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,58 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { PrimeReactProvider } from 'primereact/api'
-import 'primeicons/primeicons.css'
-import App from './App.tsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'primeicons/primeicons.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PrimeReactProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="flex justify-center items-center h-1/2 mt-[76px]">
+                <h1 className="text-2xl font-bold">Home</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/departamentos"
+            element={
+              <div className="flex justify-center items-center h-1/2 mt-[76px]">
+                <h1 className="text-2xl font-bold">Departamentos</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/departamentos/new"
+            element={
+              <div className="flex justify-center items-center h-1/2 mt-[76px]">
+                <h1 className="text-2xl font-bold">Novo Departamento</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/departamentos/edit/:id"
+            element={
+              <div className="flex justify-center items-center h-1/2 mt-[76px]">
+                <h1 className="text-2xl font-bold">Editar Departamento</h1>
+              </div>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <div className="flex justify-center items-center h-1/2 mt-[76px]">
+                <h1 className="text-2xl font-bold">Not Found</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
       <App />
     </PrimeReactProvider>
   </StrictMode>,
-)
+);
